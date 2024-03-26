6780e4d01b0b2780d91205cb035585cbc0d175be
nicoetchegoyen <nico-etchegoyen@live.com.ar>
2024-03-26 19:58:35 -0300

    comentarios de factorial.py

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -8,9 +8,10 @@
 import sys
 
 def factorial(num): 
-    if num < 0: 
+    if num < 0:  # verifica si el número es negativo
         print("Factorial de un número negativo no existe")
-    elif num == 0: 
+    elif num == 0: # retorna 1 si el número es cero
+        return 1  
         return 1
     else: 
         fact = 1
@@ -19,17 +20,18 @@ def factorial(num):
             num -= 1
         return fact 
 
-if len(sys.argv) == 1:
+if len(sys.argv) == 1: # verifica si no se proporcionaron argumentos de línea de comandos
     rango = input("Ingrese el rango (sin límite inferior -hasta o sin límite superior desde-): ")
-    if rango.startswith("-"):
+    if rango.startswith("-"):  # si el rango comienza con "-", establece el límite superior y deja el inferior en 1
+        hasta = int(rango[1:])  
         hasta = int(rango[1:])
         desde = 1
-    elif rango.endswith("-"):
+    elif rango.endswith("-"):  # si el rango termina con "-", establece el límite inferior y el superior en 60
         desde = int(rango[:-1])
         hasta = 60
-    else:
+    else:                      # si el rango está en formato "desde-hasta", divide el rango y establece los límites
         desde, hasta = map(int, rango.split('-'))
-else:
+else:                          # si se proporcionaron argumentos de línea de comandos
     rango = sys.argv[1]
     if rango.startswith("-"):
         hasta = int(rango[1:])
@@ -40,5 +42,5 @@ else:
     else:
         desde, hasta = map(int, rango.split('-'))
 
-for num in range(desde, hasta + 1):
-    print("Factorial", num, "! es", factorial(num))
+for num in range(desde, hasta + 1):   # bucle sobre el rango especificado
+    print("Factorial", num, "! es", factorial(num))  # imprime el factorial de cada número en el rango
